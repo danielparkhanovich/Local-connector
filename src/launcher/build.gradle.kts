638413b6b899ba1ba4f57dbf42087e96c4b19576
Daniel Parkhanovich <scanax2@gmail.com>
2024-06-11 15:04:46 +0200

    (DEM-232) Added postgresql persistence using TractusX migrations.

--- src/launcher/build.gradle.kts
+++ src/launcher/build.gradle.kts
@@ -54,6 +54,21 @@ dependencies {
     // DAPS - we have no DAPS now
     // implementation(libs.edc.oauth2)
 
+    // register SQL
+    implementation(libs.edc.core.spi)
+    implementation(libs.edc.sql.core)
+
+    // Adds Database-Related EDC-Extensions (EDC-SQL-Stores, JDBC-Driver, Pool and Transactions)
+    implementation(libs.edc.control.plane.sql)
+    implementation(libs.edc.transaction.local)
+    implementation(libs.postgresql)
+    implementation(libs.flyway.core)
+
+    implementation(libs.edc.sql.pool.apache.commons)
+
+    // apply migrations from tractusX
+    implementation(libs.tractus.postgresql.data.plane.migration)
+    implementation(libs.tractus.postgresql.control.plane.migration)
 }
 
 application {
